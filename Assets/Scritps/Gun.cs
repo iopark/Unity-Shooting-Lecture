0464a7d8322b2e0efcb49c21620194d1373fcf21
jungtaek kim <kgajungtaek@gmail.com>
2023-05-18 01:12:07 +0900

    feat : 파티클 시스템
    씬 안에 다수의 작은 입자을 생성하고 애니메이션하여 액체, 구름 및 불꽃 같은 유체 엔티티를 시뮬레이션함
    유니티에서 간단한 효과를 만들기 위해 사용feat : 파티클 시스템
    
    씬 안에 다수의 작은 입자을 생성하고 애니메이션하여 액체, 구름 및 불꽃 같은 유체 엔티티를 시뮬레이션함
    유니티에서 간단한 효과를 만들기 위해 사용

--- Assets/Scritps/Gun.cs
+++ Assets/Scritps/Gun.cs
@@ -6,14 +6,22 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] private float maxDistance;
     [SerializeField] private int damage;
+    [SerializeField] private ParticleSystem bulletEffect;
+    [SerializeField] private ParticleSystem muzzleEffect;
 
     public void Fire()
     {
+        muzzleEffect.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance))
         {
             IHittable target = hit.transform.GetComponent<IHittable>();
             target?.Hit(hit, damage);
+
+            ParticleSystem effect = Instantiate(bulletEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            effect.transform.parent = hit.transform.transform;
+            Destroy(effect.gameObject, 3f);
         }
     }
 }
